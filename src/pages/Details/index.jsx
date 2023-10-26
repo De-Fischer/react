16be19c37da99d6c5cbcccb159792a24386ba1bf
De-Fischer <defischer@universo.univates.br>
2023-10-26 08:44:12 -0300

    button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "./style";
 
+import { Button } from "../../components/Button";
+
 export function Details() {
    /*quando vai receber estilização usar div se não somente o fragment <></>*/
   return (
     <Container>
       <h1>Hello World</h1>
+
+      <Button/>
     </Container>   
   )
 };
