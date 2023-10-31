0c60beaa711d1bcd88218e8a199a0361c02e52b5
De-Fischer <defischer@universo.univates.br>
2023-10-31 09:17:19 -0300

    Novos componentes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,10 @@
+import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from "./style";
 
+import { Note } from "../../components/Note";
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
 
 
@@ -20,15 +24,25 @@ export function Home() {
       </Menu>
 
       <Search>
-
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
       </Search>
 
       <Content>
-
+        <Section title='Minhas notas'>
+          <Note data={{ 
+            title: 'React', 
+            tags:[
+              { id: '1', name: 'react' },
+              { id: '2', name: 'JS' }
+            ]
+          }}
+          />
+        </Section>
       </Content>
 
       <NewNote>
-
+        <FiPlus />
+        Criar nota
       </NewNote>
 
 
