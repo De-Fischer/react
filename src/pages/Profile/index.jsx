8f02a1b42ee1df1e5ecd564a304b0483218d8b23
De-Fischer <defischer@universo.univates.br>
2023-11-13 15:28:28 -0300

    Finalizando

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,7 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 
 import { Container, Form, Avatar } from "./styles";
 
